61017bf4662c445aebb24ea1cc70f359a5085e96
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-06-19 12:10:16 +0200

    add mod2-lec-1-ejercicio-6

---
+++ modulo-2-leccion-1-ejercicios-1-8/main.js
@@ -0,0 +1,4 @@
+const titleElement = document.querySelector(".title");
+const content =
+  "<h1>Lorem ipsum</h1><img src='http://via.placeholder.com/350x150'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nesciunt, reprehenderit ab beatae est culpa. Asperiores animi quos possimus odit cum ipsum excepturi facere, natus, ratione nesciunt nihil ea fugiat.</p>";
+titleElement.innerHTML = content;
