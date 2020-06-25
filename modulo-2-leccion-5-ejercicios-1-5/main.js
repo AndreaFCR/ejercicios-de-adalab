e92076e52677f92266987accc2b5df0d0d63c17a
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-06-25 10:36:39 +0200

    add mod 2 lec 5 ej 4

--- modulo-2-leccion-5-ejercicios-1-5/main.js
+++ modulo-2-leccion-5-ejercicios-1-5/main.js
@@ -32,5 +32,18 @@ lorem.addEventListener("mouseover", duplicate);
 
 // ejercicio 4
 
-console.log(window.scrollY);
-if () {}
+const container = document.querySelector(".js-bigLorem");
+
+function changeBackgroundColor() {
+  let y = window.scrollY;
+
+  if (y > 500 && y < 1000) {
+    container.classList.remove("color-first");
+    container.classList.add("color-second");
+  } else if (y > 1000) {
+    container.classList.remove("color-second");
+    container.classList.add("color-third");
+  }
+}
+
+window.addEventListener("scroll", changeBackgroundColor); //se utiliza window para que la referencia sea la ventana
