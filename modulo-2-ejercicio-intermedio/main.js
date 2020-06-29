f65599b04eca9f818ab5fd1e1f64e5056337338a
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-06-29 12:16:24 +0200

    add mod2 ex repaso intermedio completed

--- modulo-2-ejercicio-intermedio/main.js
+++ modulo-2-ejercicio-intermedio/main.js
@@ -5,12 +5,20 @@ const face = document.querySelector(".face-js");
 const select = document.querySelector(".select-js");
 const container = document.querySelector(".container-js");
 
-function handleUpdateClick(ev) {
+function handleUpdateClick() {
   //pintar cara
+  updateFace();
+  //cambiar color
+  updateBackground();
+}
+
+function updateFace() {
   const selectValue = select.value;
   face.innerHTML = selectValue;
-  //cambiar color
-  const randomNumber = parseInt(Math.random() * 100);
+}
+
+function updateBackground() {
+  const randomNumber = getRandomNumber();
   container.classList.remove("container-color-even");
   container.classList.remove("container-color-odd");
   if (randomNumber % 2 === 0) {
@@ -19,5 +27,18 @@ function handleUpdateClick(ev) {
     container.classList.add("container-color-odd");
   }
 }
+
+const getRandomNumber = () => parseInt(Math.random() * 100);
+
+// PASAMOS DESDE LAS DOS FORMAS DE FUNCTION A UNA ARROW
+// const getRandomNumber = () => {
+//   const randomNumber = parseInt(Math.random() * 100);
+//   return randomNumber;
+// }
+
+// const getRandomNumber = function () = {
+//     const randomNumber = parseInt(Math.random() * 100);
+//     return randomNumber;
+// }
 
 btn.addEventListener("click", handleUpdateClick);
