5fa0a6973664b1eef44b0af717bf7a9d9f8e920d
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-07-04 14:10:50 +0200

    add local fetch and obtain name data

--- modulo-2-ejercicio-finde-2/app/js/main.js
+++ modulo-2-ejercicio-finde-2/app/js/main.js
@@ -1,9 +1,30 @@
-'use strict';
+"use strict";
 
-const ENDPOINT = 'https://beta.adalab.es/ejercicios-extra/js-fetch-arrays-princesas-disney/data/users.json'
+const ENDPOINT = "../../data/users.json";
 
 let users = [];
 let favorites = [];
 
 /* Do your magic! 🦄🦄🦄*/
 
+fetch(ENDPOINT)
+  .then((response) => response.json())
+  .then((data) => {
+    // para pintar en el html los elementos de la lista: nombre, foto y descripcion.
+    const ulElement = document.querySelector(".js-user-list");
+    let htmlCode = "";
+
+    htmlCode += `<li class="item-js"><div class="container-js">`;
+    htmlCode += `<div class="photo-name-js"><img class="img-js" src="" alt="Foto princesa" />`;
+    htmlCode += `<h3 class="name-js">nombre</h3></div>`;
+    htmlCode += `<div class="description-js"><p>descripcion</p></div></div></li>`;
+    ulElement.innerHTML = htmlCode;
+
+    let names;
+    for (let i = 0; i < data.length; i++) {
+      names = data[i].name;
+      console.log(names);
+    }
+
+    // console.log(names);
+  });
