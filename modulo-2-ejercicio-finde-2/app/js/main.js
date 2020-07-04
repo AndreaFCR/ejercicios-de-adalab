e01c1c3521a404ac952b5871df6b9e149f0aefa2
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-07-04 14:43:46 +0200

    add max-with and flex to items

--- modulo-2-ejercicio-finde-2/app/js/main.js
+++ modulo-2-ejercicio-finde-2/app/js/main.js
@@ -10,21 +10,27 @@ let favorites = [];
 fetch(ENDPOINT)
   .then((response) => response.json())
   .then((data) => {
-    // para pintar en el html los elementos de la lista: nombre, foto y descripcion.
+    // para pintar en el html los elementos de la lista data: nombre, foto y descripcion.
     const ulElement = document.querySelector(".js-user-list");
     let htmlCode = "";
 
-    htmlCode += `<li class="item-js"><div class="container-js">`;
-    htmlCode += `<div class="photo-name-js"><img class="img-js" src="" alt="Foto princesa" />`;
-    htmlCode += `<h3 class="name-js">nombre</h3></div>`;
-    htmlCode += `<div class="description-js"><p>descripcion</p></div></div></li>`;
-    ulElement.innerHTML = htmlCode;
-
-    let names;
     for (let i = 0; i < data.length; i++) {
+      let names;
+      let photo;
+      let description;
+
       names = data[i].name;
-      console.log(names);
+      photo = data[i].picture;
+      description = data[i].comment;
+
+      htmlCode += `<li class=" item item-js">`;
+      htmlCode += `<div class="container1">`;
+      htmlCode += `<img class="img" src="${photo}" alt="Foto ${names}" />`;
+      htmlCode += `<h4 class="name-js">${names}</h4></div>`;
+      htmlCode += `<div class="container-2"><p>${description}</p></div></li>`;
+      ulElement.innerHTML = htmlCode;
     }
 
-    // console.log(names);
+    let items = document.querySelectorAll(".item-js");
+    console.log(items);
   });
