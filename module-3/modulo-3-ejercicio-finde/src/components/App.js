482099a1b892bd0540b0950d3ff1f2dd09e936b3
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-08-01 20:00:04 +0200

    add ex series in react with basic structure

---
+++ module-3/modulo-3-ejercicio-finde/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import "../stylesheets/app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ module-3/modulo-3-ejercicio-finde/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../stylesheets/header.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="header__title">Buscador de series</h1>
+    </div>
+  );
+}
+
+export default Header;
